46e6c8484cc65ed77fa96ebc88e3ffd6806426a0
Kan <kan.peljto@stu.ibu.edu.ba>
2024-03-19 21:38:42 +0100

    I have no idea what I'm doing, but I moved all the html files to one location and had to fix the styles href for it to work, also have to fix some images.

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require("path");
 
 const app = express();
 
+app.use("/static",express.static(path.resolve(__dirname,"public","static")));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public", "index.html"));
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"public", "index.html"));
 });
 
 app.listen(process.env.PORT || 5050, () => console.log("Server running."));
