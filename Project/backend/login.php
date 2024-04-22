d29d583db26ae212706a67c230602184fa97b44a
KanPeljto <116578415+KanPeljto@users.noreply.github.com>
2024-04-22 19:40:20 +0200

    Cannot implement click counting, trying

--- Project/backend/login.php
+++ Project/backend/login.php
@@ -13,6 +13,7 @@ $user=$user_service->login_user($email,$password);
 
 if($user){
     echo 'Login successuful!';
+    header("Location: http://localhost/KanProjectWeb/Project/public/index.html");
 } else {
     echo 'Login failed';
 }
